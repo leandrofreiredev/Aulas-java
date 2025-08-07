61f71602b278b254c277bbf380ede45e343e53fc
leandrofreiredev <leandrofreire.dev@gmail.com>
2025-08-07 13:07:04 -0300

    tipos de variáveis e suas pecularidades

---
+++ src/edu/leandro/TipoVariaveis/Constantes.java
@@ -0,0 +1,18 @@
+package edu.leandro.TipoVariaveis;
+
+public class Constantes {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;// garante que a constante não seja alterada
+                                        // mesmo utilizando caixa alta"AAAA"
+
+       //Para declarar uma vaiável como constante ,utilizamos a palavra "final",
+        //mas por convenção esta variavel deverá ser escrita em caixa alta"AAA"
+
+
+
+    }
+}
---
+++ src/edu/leandro/TipoVariaveis/TipoVariaveis.java
@@ -0,0 +1,19 @@
+package edu.leandro.TipoVariaveis;
+
+public class TipoVariaveis {
+    public static void main(String[] args) {
+
+        //TIPOS PRIMITIVOS
+
+        byte Idade = 123; // tipo numero
+        short ano = 2020;// tipo numero inteiro
+        int cep = 54745822;// tipo numero
+        long cpf = 630000555845L;// se é "tipo" long tem que encerrar com "L"
+        float pi = 3.14F;// se é tipo "float" tem que encerrar com "F"
+        double salario= 1500.50;
+
+        // CLASSE "STRING" QUE REPRESENTA TEXTO NA APLICAÇÃO
+        String meuNome = "lEO";
+
+    }
+}
