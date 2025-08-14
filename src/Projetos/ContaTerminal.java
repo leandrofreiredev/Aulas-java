d1a843cba3a00ef3be6e78aeaaa0062f11ef0e9e
leandrofreiredev <leandrofreire.dev@gmail.com>
2025-08-14 10:28:35 -0300

    Projeto saldo conta bancaria.

---
+++ src/Projetos/ContaTerminal.java
@@ -0,0 +1,34 @@
+package Projetos;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        double saldo = 1.500;
+
+        System.out.println("Digite seu Nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite susa Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o numero da sua conta: ");
+        int conta = scanner.nextInt();
+
+        // imprimindo os dados .
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d, e seu saldo é R$: %.3f, que se encontra disponível para saque.\n",
+                nome, agencia, conta, saldo);
+
+
+
+
+
+
+
+    }
+
+}
