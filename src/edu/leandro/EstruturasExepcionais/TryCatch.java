d1a77e4d0491dd959754a2db034ac89330a70e85
leandrofreiredev <leandrofreire.dev@gmail.com>
2025-08-18 11:13:54 -0300

    Estruturas try catch.

---
+++ src/edu/leandro/EstruturasExepcionais/TryCatch.java
@@ -0,0 +1,41 @@
+package edu.leandro.EstruturasExepcionais;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TryCatch {
+
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome:");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome:");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade:");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura:");
+            double altura = scanner.nextDouble();
+
+
+            // IMPRIMINDO DADOS
+
+            System.out.println("ola, me chamo " + nome.toUpperCase() + " " + sobrenome);
+            System.out.println("tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + " cm ");
+            scanner.close();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Os campos de idade e altura precisam ser numericos");
+        }
+    }
+
+
+
+
+}
